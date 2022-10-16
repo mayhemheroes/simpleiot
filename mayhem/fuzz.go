70bfa56d0cb9ec1844910e6bcec45b25ff0eb56c
bmwtsn098 <bmwtsn99@gmail.com>
2022-10-15 23:40:27 -0700

    add GitHub action to launch Mayhem

---
+++ mayhem/fuzz.go
@@ -0,0 +1,32 @@
+package fuzz
+
+import "strconv"
+import "github.com/simpleiot/simpleiot/api"
+
+func mayhemit(bytes []byte) int {
+
+    var num int
+    if len(bytes) < 1 {
+        num = 0
+    } else {
+        num, _ = strconv.Atoi(string(bytes[0]))
+    }
+    switch num {
+        case 1:
+            content := string(bytes)
+            var test api.Key
+            test.NewToken(content)
+            return 0
+
+        default:
+            content := string(bytes)
+            var test api.Key
+            test.ValidToken(content)
+            return 0
+    }
+}
+
+func Fuzz(data []byte) int {
+    _ = mayhemit(data)
+    return 0
+}
